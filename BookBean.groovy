50b2f7341f44514559eefed72a19b40d89f5fcc5
Jerry Wang <jerry.wang@delphix.com>
2016-04-14 23:03:03 -0700

    update BookBean.groovy

--- BookBean.groovy
+++ BookBean.groovy
@@ -1,13 +1,20 @@
 // Demonstration of GroovyBean
 
 class Book {
+	// Property declaration
     String title
 }
 
 Book gina = new Book()
+
+// Property use with explicit accessor methods
 gina.setTitle('Groovy in Action')
 assert gina.getTitle() == 'Groovy in Action'
 assert getTitleBackwards(gina) == 'noitcA ni yvoorG'
+
+// Property use with Groovy shortcuts
+gina.title = 'Groovy conquers the world!'
+assert gina.title == 'Groovy conquers the world!'
 
 String getTitleBackwards(book) {
     String title = book.getTitle()
