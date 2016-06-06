4524afb34fe1f40d8f305f8f2cba9cf911aa4fac
Jerry Wang <jerry.wang.jw2@roche.com>
2016-06-06 00:20:09 -0700

    update closure_delegate.groovy

--- src/main/groovy/me/jerrywang/example/closure_delegate.groovy
+++ src/main/groovy/me/jerrywang/example/closure_delegate.groovy
@@ -1,28 +1,33 @@
 package me.jerrywang.example
 
 class Post {
-    int count
-    def info() {
-        "This is blog post #$count!"
-    }
+  int count
+  def info() {
+    "This is blog post #$count!"
+  }
 }
 
 count = 0
 
 def info() {
-    "Count value is $count."
+  "Count value is $count."
 }
 
+// Declare a closure called getInfo
 def getInfo = {
-    println this.getClass()
-    println delegate.getClass()
-    count++
-    info()
+  println "this is: " + this.class
+  println "delegate is: " + delegate.class
+  count++
+  info()
 }
 
+println "Current resolveStrategy of getInfo: " + ['OWNER_FIRST', 'DELEGATE_FIRST'][getInfo.resolveStrategy]
+// Call closure getInfo with default OWNER_FIRST resolveStrategy
 println getInfo()
 
+println "Setting resolveStrategy of getInfo to DELEGATE_FIRST..."
 getInfo.resolveStrategy = Closure.DELEGATE_FIRST
+println "Current resolveStrategy of getInfo: " + ['OWNER_FIRST', 'DELEGATE_FIRST'][getInfo.resolveStrategy]
 getInfo.delegate = new Post(count: 100)
-
+// Call closure getInfo with DELEGATE_FIRST resolveStrategy
 println getInfo()
